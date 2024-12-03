4f6bb41cde7dc7de5e39579e2b85121bdccc92a7
wassupdee <daichihirohata@DESKTOP-GDIBLU3>
2024-12-03 11:01:47 +0900

    Google Maps APIを用いた地図表示

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <div>
+        <label>Latitude: </label>
+        <input type="text" v-model="latitude" />
+      </div>
+      <div>
+        <label>Longitude: </label>
+        <input type="text" v-model="longitude" />
+      </div>
+    </form>
+    <button class="btn" style="width: 200px; background: skyblue;" v-on:click="showMap">SHOW</button>
+  </div>
+  <div ref="map" id="mapArea" style="height: 400px;"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: () => ({
+    latitude: 35.46248486750884, // 初期緯度
+    longitude: 139.6231033539685, // 初期経度
+  }),
+  methods: {
+    showMap() {
+      const apiKey = process.env.VUE_APP_GOOGLE_MAPS_API_KEY; // 環境変数からAPIキーを取得
+      // APIキーを使用しスクリプトを呼び出す
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      // スクリプト呼び出し後のコールバック
+      window.initMap = () => {
+        const myLatLng = { lat: Number(this.latitude), lng: Number(this.longitude) };
+        const map = new window.google.maps.Map(this.$refs.map, {
+          center: myLatLng,
+          zoom: 12,
+        });
+        new window.google.maps.Marker({
+          position: myLatLng,
+          map
+        });
+      };
+    },
+  },
+};
 </script>
 
 <style>
